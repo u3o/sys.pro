be3bf90396b55c235ffac321b3fea9f7c66a522e
u3o <64749081+u3o@users.noreply.github.com>
2024-11-05 23:39:35 +0700

    tests for task2.py

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ def solution(a, b):
         res.append((a[i], b[i]))
     return res
 
-a = [1, 2, 3]
-b = ["a", "b"]
-
-print(solution(a, b))
+assert solution([1, 2, 3], ["a", "b"]) == [(1, "a"), (2, "b")]
+assert solution([1, 2], ["a", "b", "c", "d"]) == [(1, "a"), (2, "b")]
+assert solution(["a", 2, 3.5], [-3, "c", "c", "c", "c"]) == [("a", -3), (2, "c"), (3.5, "c")]
+assert solution([], []) == []
