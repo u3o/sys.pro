6afc8b52d45f133abb92b79794f3a79d40eae557
u3o <64749081+u3o@users.noreply.github.com>
2024-11-05 23:46:08 +0700

    now task3.py uses float values

--- task3.py
+++ task3.py
@@ -1,19 +1,13 @@
-import pytest
-
-a = ""
-
 def solution(a):
     arr = a.split(" | ")
 
     for i in range(len(arr)):
-        arr[i] = arr[i].split()
+        arr[i] = [float(i) for i in arr[i].split()]
     
     return arr
 
-arr = solution(a)
-
 #tests
-assert solution("1 2 | 3 4") == [['1', '2',], ['3', '4']]
-assert solution("1 2 3 | 4 5 | 6") == [['1', '2', '3'], ['4', '5'], ['6']]
+assert solution("1.1 2.5 | 3 4.0") == [[1.1, 2.5,], [3.0, 4.0]]
+assert solution("1 2 3 | 4 5 | 6") == [[1.0, 2.0, 3.0], [4.0, 5.0], [6.0]]
 assert solution("") == [[]]
 assert solution(" | ") == [[], []]
