168a271539056aeae8d4a0cc40d1c4113dd0453f
u3o <64749081+u3o@users.noreply.github.com>
2024-11-13 01:00:03 +0700

    now task1.py works for numbers greater than 2**64

--- task1.py
+++ task1.py
@@ -1,32 +1,23 @@
-def bi(n):
-    N = abs(n)
-    res = 0
-    count = 0
-    while (N > 0):
-        res = (10**count * (N % 2)) + res
-        N = N // 2
-        count += 1
-    if (res >= 0):
-        return res
+def solution(num):
+    def foo(n):
+        i = 1
+        while True:
+            if n < 2**i:
+                return i
+            i += 1
+
+    c = 0
  
-    else:
-        return ()
- 
-def foo(n):
-    for i in range(1, 65):
-        if n < 2**i:
-            return i
- 
-print(bi(123))
-print(bi(~123))
-num = int(input("Enter a number... "))
-c = 0
- 
-if num < 0:
-    num = (abs(num))^(2**foo(abs(num))-1)-1
-    c += 1
-print(bi(num))
-while (num > 1):
-    c += num % 2
-    num = num // 2
-print(c+1)
+    if num < 0:
+        num = (abs(num))^(2**foo(abs(num))-1)-1
+        c += 1
+
+    while (num > 1):
+        c += num % 2
+        num = num // 2
+
+    return c+1
+
+#tests
+assert solution(-123) == 3
+assert solution(10) == 2
