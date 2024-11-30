15cbd34a0f2c1f59be89a0a6ed86fc9afc006828
u3o <64749081+u3o@users.noreply.github.com>
2024-11-30 22:39:04 +0700

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,28 +1,29 @@
-import itertools
-import numpy
-
-def take(arr, count):
+def take(iterable, amount):
     c = 0
-    for i in arr:
-        if c < count:
+    for i in iterable:
+        if c < amount:
             yield i 
             c += 1
         else:
             break
-
-#wow
+#part 1
 def cycle(iterable):
     while True:
         for i in iterable:
             yield i
-
+#"tests"
 print(list(take(cycle([1,2,3]), 10)))
+g = cycle(['a', 'b'])
+for i in range(10):
+    print(next(g))
 
-g = cycle([1, 2])
-print(next(g))
-print(next(g))
-print(next(g))
-print(next(g))
+#part 2
+def chain(*iterables):
+    for iterable in iterables:
+        yield iterable
+
+#"tests"
+g = chain('a', [1, 2], 'b')
 print(next(g))
 print(next(g))
 print(next(g))
